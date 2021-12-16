63e0888f74d5f683779eded4d9fbde4f6521ded4
zamtechie <zameer.inamdar2008@gmail.com>
2021-12-16 17:21:38 +0530

    Initial commit for explore-js repo

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Learning</h1>
-<lable>1.  Hello!!!<lable>`;
+<label>1. Welcome !!!</label><br>
+<label>2. Fibonacci Series</label><br>
+<label>3. Subset of array [1,2,3,1,3,4,5,6,7] from 3 to 7 position</label><br>
+<label>4. Remove the duplicate values form the array [1,2,3,1,3,4,5,6,7]</label>`;
 
 function helloWorld() {
   console.log('Welcome to the world of JS using Stackblitz');
