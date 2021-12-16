82ccdd2a44438e46a1de9ecff5002f2e3b3a206c
zamtechie <zameer.inamdar2008@gmail.com>
2021-12-16 17:13:11 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# explore-js
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/explore-js)
---
+++ index.js
@@ -0,0 +1,13 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Learning</h1>
+<lable>1.  Hello!!!<lable>`;
+
+function helloWorld() {
+  console.log('Welcome to the world of JS using Stackblitz');
+}
+
+helloWorld();
