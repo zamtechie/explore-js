cb3a56941ac79d72255081a0174c396cd3dba1ad
zamtechie <zameer.inamdar2008@gmail.com>
2021-12-16 20:44:14 +0530

    Added 'Call, Apply and Bind' methods

--- index.js
+++ index.js
@@ -3,14 +3,43 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Learning</h1>
-<label>1. Welcome !!!</label><br>
-<label>2. Fibonacci Series</label><br>
-<label>3. Subset of array [1,2,3,1,3,4,5,6,7] from 3 to 7 position</label><br>
-<label>4. Remove the duplicate values form the array [1,2,3,1,3,4,5,6,7]</label>`;
+appDiv.innerHTML = `<h1>JS Learning</h1>`;
+appDiv.innerHTML += `<ol>
+  <li>HelloWorld</li>
+  <li>Call, Apply & Bind</li>
+  <li>Fibonacci Series</li>
+  <li>Subset of array [1,2,3,1,3,4,5,6,7] from 3 to 7 position</li>
+  <li>Remove the duplicate values form the array [1,2,3,1,3,4,5,6,7]</li>
+</ol>`;
+
+/********************************/
+/********* HelloWorld ***********/
+/********************************/
 
 function helloWorld() {
   console.log('Welcome to the world of JS using Stackblitz');
 }
-
 helloWorld();
+
+/********************************/
+/* Call, Apply and Bind Methods */
+/********************************/
+var person1 = { firstName: 'Ranga', lastName: 'Komarthi' };
+var person2 = { firstName: 'Zameer', lastName: 'Inamdar' };
+
+function say(greeting) {
+  console.log(greeting + ' ' + this.firstName + ' ' + this.lastName);
+}
+
+say.call(person1, 'Call-1', 'Call-2');
+say.call(person2, 'Call-1', 'Call-2');
+
+say.apply(person1, ['Apply-1', 'Apply-2']);
+say.apply(person2, ['Apply-1', 'Apply-2']);
+
+let sayBind = say.bind(person1);
+sayBind(['Bind1', 'Bind2']);
+
+/********************************/
+/* Call, Apply and Bind Methods */
+/********************************/
